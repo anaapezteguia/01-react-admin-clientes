9f1d3ed5e322f4de7e5591619bb2d6d1dc5fb1a0
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-06-03 14:21:08 +0200

    Add states

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  // crear citas
+  const [cita, setCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+  });
+  // actualizar inputs
+  const handleChange = (ev) => {
+    setCita({
+      ...cita,
+      [ev.target.name]: ev.target.value,
+    });
+  };
+  // extraer valores con destructuring
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
   return (
     <>
       <h2>Concertar cita</h2>
@@ -11,6 +29,8 @@ const Form = () => {
           name="mascota"
           className="u-full-width"
           placeholder="Nombre de tu mascota"
+          onChange={handleChange}
+          value={mascota}
         />
         <label>Dueño</label>
         <input
@@ -18,13 +38,32 @@ const Form = () => {
           name="propietario"
           className="u-full-width"
           placeholder="Nombre del dueño"
+          onChange={handleChange}
+          value={propietario}
         />
         <label>Fecha</label>
-        <input type="date" name="fecha" lassName="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+          value={fecha}
+        />
         <label>Hora</label>
-        <input type="time" name="hora" lassName="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+          value={hora}
+        />
         <label>Describe los síntomas</label>
-        <textarea name="sintomas" className="u-full-width"></textarea>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={handleChange}
+          value={sintomas}
+        ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Concertar cita
         </button>
